95c4da092232dbe87352c55cdd2bd2bff30a2188
Michael McNamara <michael.mcnamara@theguardian.com>
2016-02-17 18:54:19 +0000

    Update S3Operations.scala

--- src/main/scala/app/api-utils/S3Operations.scala
+++ src/main/scala/app/api-utils/S3Operations.scala
@@ -40,7 +40,7 @@ class S3Operations(s3BucketName: String, configFile: String) {
     val emailPassword = conf.getString("email.password")
     if ((contentApiKey.length > 0) && (wptBaseUrl.length > 0) && (wptApiKey.length > 0)){
       println(DateTime.now + " Config retrieval successful. \n You are using the following webpagetest instance: " + wptBaseUrl)
-      val returnArray = Array(contentApiKey, wptBaseUrl, wptApiKey, wptLocation)
+      val returnArray = Array(contentApiKey, wptBaseUrl, wptApiKey, wptLocation, emailUsername, emailPassword)
       returnArray
     }
     else {
